c36f24f397199aafcbc155ab797b2b0558d9e7bd
Vyacheslav Allabergenov <slawushkin@mail.ru>
2024-12-28 15:51:46 +0300

    Update module_6_1.py

--- module_6_1.py
+++ module_6_1.py
@@ -1,75 +1,98 @@
-# Задача "Съедобное, несъедобное":
-
-# Примечания:
-# Помните, обращаясь к атрибутам объекта текущего класса используйте параметр self.
-# Передавая объекты классов Fruit и Flower в метод eat, так же можно обращаться к их атрибутам внутри.
-# Обращайте внимание на то, где атрибут класса, а где атрибут объекта.
-
 # Создайте:
 # 2 класса родителя: Animal, Plant
-# Для класса Animal атрибуты alive = True(живой) и fed = False(накормленный), name - индивидуальное
-# название каждого животного.
-class Animal:
-    def __init__(self, name):
+# Для класса Animal атрибуты alive = True(живой) и fed = False(накормленный), name - индивидуальное название каждого животного.
+# Для класса Plant атрибут edible = False(съедобность), name - индивидуальное название каждого растения
+#
+# 4 класса наследника:
+# Mammal, Predator для Animal.
+# Flower, Fruit для Plant.
+#
+# У каждого из объектов класса Mammal и Predator должны быть атрибуты и методы:
+# eat(self, food) - метод, где food - это параметр, принимающий объекты классов растений.
+# В данном случае можно использовать принцип наследования, чтобы не дублировать код.
+#
+# Метод eat должен работать следующим образом:
+# Если переданное растение (food) съедобное - выводит на экран "<self.name> съел <food.name>", меняется атрибут fed на True.
+# Если переданное растение (food) не съедобное - выводит на экран "<self.name> не стал есть <food.name>", меняется атрибут alive на False.
+# Т.е если животному дать съедобное растение, то животное насытится, если не съедобное - погибнет.
+#
+# У каждого объекта Fruit должен быть атрибут edible = True (переопределить при наследовании)
+#
+# Создайте объекты классов и проделайте действия затронутые в примере результата работы программы.
+#
+# Пункты задачи:
+# Создайте классы Animal и Plant с соответствующими атрибутами и методами
+# Создайте(+унаследуйте) классы Mammal, Predator, Flower, Fruit с соответствующими атрибутами и методами. При необходимости переопределите значения атрибутов.
+# Создайте объекты этих классов.
 
+class Animal:
+
+    def __init__(self, name):
+        self.name = name
         self.alive = True
         self.fed = False
-        self.name = name
 
-    # Если переданное растение (food) съедобное - выводит на экран "<self.name> съел <food.name>",
-    # меняется атрибут fed на True.
+
+class Plant:
+
+    def __init__(self, name):
+        self.name = name
+        self.edible = False
+
+
+class Mammal(Animal):
+
     def eat(self, food):
-        if food.edible == True:
+        if food.edible:
             print(f'{self.name} съел {food.name}')
             self.fed = True
-        # Если переданное растение (food) не съедобное - выводит на экран "<self.name> не стал есть <food.name>",
-        # меняется атрибут alive на False.
         else:
-            print(f"{self.name} не стал есть {food.name}")
+            print(f'{self.name} не стал есть {food.name}')
             self.alive = False
 
 
-# Т.е если животному дать съедобное растение, то животное насытится, если не съедобное - погибнет.
+class Predator(Animal):
+    def eat(self, food):
+        if food.edible:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False
 
-# Для класса Plant атрибут edible = False(съедобность), name - индивидуальное название каждого растения
-class Plant:
+
+class Flower(Plant):
+    pass
+
+
+class Fruit(Plant):
     def __init__(self, name):
-        self.edible = False # несъедобный
         self.name = name
+        self.edible = True
 
 
-class Mammal(Animal):  # Наследует атрибуты и методы из класса родителя
-    pass
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
 
+print(a1.name)
+print(p1.name)
 
-class Predator(Animal):  # Наследует атрибуты и методы из класса родителя
-    pass
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
 
+# Вывод на консоль:
 
-class Flower(Plant):  # Наследует атрибуты и методы из класса родителя
-    pass
-
-
-class Fruit(Plant):  # У каждого объекта Fruit должен быть атрибут
-    # edible = True (переопределить при наследовании)
-    def __init__(self, name):
-        self.edible = True  # съедобный
-        self.name = name
-
-
-if __name__ == '__main__':
-    a1 = Predator('Волк с Уолл-Стрит')
-    a2 = Mammal('Хатико')
-    p1 = Flower('Цветик семицветик')
-    p2 = Fruit('Заводной апельсин')
-
-    print(a1.name)
-    print(p1.name)
-
-    print(a1.alive)
-    print(a2.fed)
-    a1.eat(p1)
-    a2.eat(p2)
-    print(a1.alive)
-    print(a2.fed)
-# Что произошло: Хищник попытался съесть цветок и погиб, млекопитающее съело фрукт и насытилось.
+# Волк с Уолл-Стрит
+# Цветик семицветик
+# True
+# False
+# Волк с Уолл-Стрит не стал есть Цветик семицветик
+# Хатико съел Заводной апельсин
+# False
+# True
